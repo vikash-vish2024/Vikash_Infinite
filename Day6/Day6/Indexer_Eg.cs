cd30c380da03db0da3aaf005d3ad4eb519bc28e8
Vikash Vishwakarma <vikashv@infinite.com>
2024-02-29 13:16:34 +0530

    pre Lunch Training Stuff

---
+++ Day6/Day6/Indexer_Eg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    class Indexer_Eg
+    {
+        private string[] names = new string[3];
+        
+        //indexer declaration
+        public string this[int f]
+        {
+            get { return names[f]; }
+
+            set { names[f] = value; }
+
+        }
+        public static void Main()
+        {
+            Indexer_Eg indexer_Eg = new Indexer_Eg();// its has virtual array inside it
+            indexer_Eg[0] = "Vikash";// setting the value of names of 0
+            indexer_Eg[1] = "Avinash";
+            indexer_Eg[2] = "Basavraj";
+            Console.WriteLine($"{indexer_Eg[0]} -> {indexer_Eg[1]} -> {indexer_Eg[2]}");
+        }
+    }
+}
---
+++ Day6/Day6/Properties_eg.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    class Employee
+    {
+        string _code = "N.A";
+        string _Name = "Missing";
+        int _Age = 0;
+        //Autometic property implementation
+        //public double _Salary { protected get; set; } = 26000;
+        public double _Salary { get; } = 26000;
+
+        // override the object.tostring fun to show all the fun in one go
+        public override string ToString()
+        {
+            return "Employee code ->" + Code+ " Name->"+Name+", Age is - "+Age+", Salary is ->"+_Salary;
+        }
+        //declare properties of private field
+        public string Code
+        {
+            get { return _code; } //reads the values og the field
+            set { _code = value; } //for setting the value from the field
+        }
+        public string Name
+        {
+            get { return _Name; }
+            //setting the value using condition
+            set
+            { if (value != " ")
+                    _Name = value;
+              else
+                    Console.WriteLine("Invalid Data");
+
+            }
+        }
+        public int Age
+        {
+            get { return _Age; } //only Display the value no changes are allowed
+        }
+    }
+    class Properties_eg
+    {
+        public static void Main()
+        {
+            Employee emp = new Employee();
+            Console.WriteLine(emp.Code+" "+emp.Name+" "+emp.Age);
+            emp.Code = "E01"; //setter
+            //Console.WriteLine(emp.Code);//getter
+            //Console.WriteLine(emp.Age);
+            emp.Name = "Vikash";
+            //Console.WriteLine(emp.Name);
+            Console.WriteLine("----Employee Details---- \n{0}",emp.ToString());
+            Console.Read();
+            
+        }
+        
+    }
+}
