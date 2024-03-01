9a15a1dd135331ac0e0702da4b70d125df5029b1
Vikash Vishwakarma <vikashv@infinite.com>
2024-03-01 11:03:07 +0530

    Prebreak Training

---
+++ Day7/Day7/Operator_Overload.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet_Day7
+{
+    class Travel
+    {
+        public int Dist { get; set; }
+        public DateTime Traveldate { get; set; }
+
+        //overload a '+' operator in order to be able to add 2 distances
+        //some rules to be followed
+        public static Travel operator +(Travel tr1, Travel tr2)
+        {
+            Travel temp = new Travel();
+            temp.Dist = tr1.Dist + tr2.Dist;
+            return temp;
+        }
+
+    }
+    class Operator_Overload
+    {
+        public static void Main()
+        {
+            Travel t1 = new Travel();
+            Travel t2 = new Travel();
+            t1.Dist = 45;
+            t2.Dist = 20;
+            Travel t3 = t1 + t2;
+            Console.WriteLine("The total Distance To Travel is : " + t3.Dist);
+            Console.Read();
+        }
+    }
+}
---
+++ Day7/Day7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet_Day7
+{
+
+    abstract class Shapes
+    {
+        public int sample { get; set; }
+        //abstract method
+        abstract public int Area();
+        public void method1() { }
+        public virtual void DrawShape()
+        {
+            Console.WriteLine("It is the Shape");
+        }
+    }
+
+    class Square : Shapes
+    {
+        int side = 0;
+        public Square(int n)
+        {
+            side = n;
+        }
+
+        public override int Area()
+        {
+            return side * side;
+        }
+
+        public override void DrawShape()
+        {
+            base.DrawShape();
+            Console.WriteLine("This is Square Shape");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shapes s = new Square(4);
+            Console.WriteLine("Area of Square : " + s.Area());
+            s.DrawShape();
+            Console.Read();
+
+        }
+    }
+}
